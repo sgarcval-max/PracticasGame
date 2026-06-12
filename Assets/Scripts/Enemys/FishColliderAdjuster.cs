60b02a9ddf259c6c8b943cce45175431d3103d1f
sgarcval-max <sgarcval@gmail.com>
2026-06-12 21:34:41 +0200

    Update

---
+++ Assets/Scripts/Enemys/FishColliderAdjuster.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class FishColliderAdjuster : MonoBehaviour
+{
+    private PolygonCollider2D col;
+    private SpriteRenderer sr;
+    private Sprite lastSprite;
+    private bool lastFlipX;
+
+    void Awake()
+    {
+        col = GetComponent<PolygonCollider2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (col == null || sr == null || sr.sprite == null) return;
+
+        if (sr.sprite != lastSprite || sr.flipX != lastFlipX)
+        {
+            lastSprite = sr.sprite;
+            lastFlipX = sr.flipX;
+            UpdateColliderShape();
+        }
+    }
+
+    void UpdateColliderShape()
+    {
+        Sprite sprite = sr.sprite;
+        int shapeCount = sprite.GetPhysicsShapeCount();
+
+        if (shapeCount == 0) return;
+
+        col.pathCount = shapeCount;
+
+        var points = new System.Collections.Generic.List<Vector2>();
+
+        for (int i = 0; i < shapeCount; i++)
+        {
+            points.Clear();
+            sprite.GetPhysicsShape(i, points);
+
+            if (sr.flipX)
+            {
+                for (int j = 0; j < points.Count; j++)
+                    points[j] = new Vector2(-points[j].x, points[j].y);
+            }
+
+            col.SetPath(i, points.ToArray());
+        }
+    }
+}
